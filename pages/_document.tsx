faeae2bba3057dc3aede6762744fea149e1a1c41
佐藤太一 <3101taichi@gmail.com>
2023-06-19 22:18:05 +0900

    zennのマークダウンを有効に

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+       <script src="https://embed.zenn.studio/js/listen-embed-event.js"></script> // 一部の埋め込みで、高さ調節のためにスクリプトを利用します
+      </Head>
       <body>
         <Main />
         <NextScript />
