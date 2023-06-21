eb6c7e6c440b9e8fecb7f0e183721e8ea896c14f
佐藤太一 <3101taichi@gmail.com>
2023-06-21 20:10:41 +0900

    不要な記述を削除

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,6 +1,5 @@
 import Container from './container'
 import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
 import { BLOG_NAME } from '../lib/constants'
 
 
--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -1,31 +1,22 @@
-import Avatar from './avatar'
 import DateFormatter from './date-formatter'
-import CoverImage from './cover-image'
 import Link from 'next/link'
 import type Author from '../interfaces/author'
 
 type Props = {
   title: string
-  // coverImage: string
   date: string
   excerpt: string
-  author: Author
   slug: string
 }
 
 const PostPreview = ({
   title,
-  // coverImage,
   date,
   excerpt,
-  // author,
   slug,
 }: Props) => {
   return (
     <div>
-      {/* <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} />
-      </div> */}
       <div className="text-lg mb-4">
         <DateFormatter dateString={date} />
       </div>
@@ -39,7 +30,6 @@ const PostPreview = ({
         </Link>
       </h3>
       <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      {/* <Avatar name={author.name} picture={author.picture} /> */}
     </div>
   )
 }
